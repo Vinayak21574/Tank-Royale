05fa966710269b2696b8230308cc772d1cc44ded
Aditya <aditya21511@iiitd.ac.in>
2022-12-08 01:22:39 +0530

    modified terrain

--- core/src/com/mygdx/game/Terrain.java
+++ core/src/com/mygdx/game/Terrain.java
@@ -7,12 +7,14 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import java.util.ArrayList;
 import java.util.Random;
 
+import static java.lang.Math.abs;
+
 public class Terrain {
     ArrayList<Integer> list=new ArrayList<>();
     MyGdxGame game;
     Texture rock=new Texture("Terrain\\Rock.png");
-//    Sprite Rock=new Sprite(rock);
-    Sprite Rock = new Sprite(rock);
+    Sprite Rock=new Sprite(rock);
+//    Sprite Rock = new Sprite(rock);
     int height=0;
 
     public Terrain(MyGdxGame game){
@@ -30,28 +32,32 @@ public class Terrain {
                 height+=1;
             }
             else{
-                height+=1+rnd.nextInt(1);
+                height+=-1+rnd.nextInt(3);
             }
             list.add(height);
         }
-//        list.set(800,list.get(800)-10);
-//        list.set(799,list.get(799)-5);
-//        list.set(801,list.get(801)-5);
-//        list.set(802,list.get(802)-2);
-//        list.set(798,list.get(798)-2);
     }
 
     void Draw(){
 //        Sprite Body=new Sprite(body,0,0, body.getWidth(), body.getHeight());
         for(int i=0;i<1600;i++){
             Rock.setPosition(i,0);
-            Rock.setScale(1,list.get(i)/rock.getHeight());
 //            Rock.setPosition(i, list.get(i));
+            Rock.setScale(1,list.get(i));
 //            Gdx.app.log(String.valueOf(5*i),String.valueOf(height));
 //            height+=list.get(i);
             Rock.draw(game.batch);
+            if (Gdx.input.justTouched()){
+                mutilation(Gdx.input.getX(), 10);
+            }
         }
 //        done=true;
+    }
+    void mutilation(int x, int m){
+        float radius = 40;
+        for (int i = x - m; i <= x+m ; i++){
+            list.set(i, (int) (list.get(i)-radius/abs(x-i)));
+        }
     }
 
 }
